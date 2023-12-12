987a58a5d2fd4c53642250413ca47b795524243d
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-12-12 18:27:30 +0100

    initial commit

---
+++ app/src/main/java/com/example/textaudioai/player/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.textaudioai.player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.textaudioai.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
--- app/src/main/java/com/example/textaudioai/voices/VoiceListActivity.kt
+++ app/src/main/java/com/example/textaudioai/voices/VoiceListActivity.kt
@@ -1,9 +1,10 @@
-package com.example.textaudioai
+package com.example.textaudioai.voices
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.textaudioai.R
 
-class MainActivity : AppCompatActivity() {
+class VoiceListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
