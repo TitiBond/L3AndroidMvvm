329af02959daeb403e5a8d4876b5197cf5bf9c88
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-12-13 11:28:20 +0100

    feat(view-model): Create the view model of the player activity (wip bunch of todos)

---
+++ app/src/main/java/com/example/textaudioai/player/PlayerViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.textaudioai.player
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+sealed class PlayerViewState {
+    object Loading : PlayerViewState();
+    data class Success(val player: Player): PlayerViewState();
+    data class Error(val message: String): PlayerViewState();
+}
+
+class PlayerViewModel: ViewModel() {
+    // TODO: This view model require an access to the database for fetching the player from the id
+    val stateLiveData = MutableLiveData<PlayerViewState>();
+
+    fun loadPlayer(id: Int) {
+        stateLiveData.value = PlayerViewState.Loading;
+
+        // TODO: Add a BDD call with Pepper and retrieve the player from the id
+        val player = Player(id, "Random title", "http://smt", "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ", "2023");
+
+        stateLiveData.value = PlayerViewState.Success(player);
+    }
+
+    // TODO: Player management play/pause/stream from player.url
+}
