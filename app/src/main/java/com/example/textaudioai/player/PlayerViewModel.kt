93aaa9305d2fbfaff19d6c9f0924aba329e3961d
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-12-13 16:49:19 +0100

    feat(player-view-model): Implement the necessary functionality for the media player and add a state Idle/Started/Stopped

--- app/src/main/java/com/example/textaudioai/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/textaudioai/player/PlayerViewModel.kt
@@ -1,26 +1,62 @@
 package com.example.textaudioai.player
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.textaudioai.player.media.MediaPlayerCustom
 
 sealed class PlayerViewState {
     object Loading : PlayerViewState();
     data class Success(val player: Player): PlayerViewState();
-    data class Error(val message: String): PlayerViewState();
+    data class Error(val message: Int): PlayerViewState();
 }
 
-class PlayerViewModel: ViewModel() {
+sealed class MediaPlayerState {
+    object Idle: MediaPlayerState();
+    object Started : MediaPlayerState();
+
+    object Stopped: MediaPlayerState();
+}
+
+
+class PlayerViewModel(): ViewModel() {
     // TODO: This view model require an access to the database for fetching the player from the id
-    val stateLiveData = MutableLiveData<PlayerViewState>();
+
+    lateinit var player: MediaPlayerCustom;
+    val playerStateLiveData = MutableLiveData<PlayerViewState>(PlayerViewState.Loading);
+    val mediaPlayerStateLiveData = MutableLiveData<MediaPlayerState>(MediaPlayerState.Idle);
 
     fun loadPlayer(id: Int) {
-        stateLiveData.value = PlayerViewState.Loading;
-
         // TODO: Add a BDD call with Pepper and retrieve the player from the id
-        val player = Player(id, "Random title", "http://smt", "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ", "2023");
+        val player = Player(id, "Random title", "/to/file/path", "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ", "2023");
 
-        stateLiveData.value = PlayerViewState.Success(player);
+        playerStateLiveData.value = PlayerViewState.Success(player);
     }
 
-    // TODO: Player management play/pause/stream from player.filePath
+    fun loadMediaPlayer(filePath: String) {
+        player.loadFile(filePath);
+    }
+
+    fun handlePlaybackState() {
+        if (player.isPlaying()) {
+            stopPlayback(false);
+        } else {
+            startPlayback();
+        }
+    }
+
+    private fun startPlayback() {
+        player.start();
+        mediaPlayerStateLiveData.value = MediaPlayerState.Started;
+    }
+
+    fun stopPlayback(rewind: Boolean) {
+        player.stop(rewind);
+        mediaPlayerStateLiveData.value = MediaPlayerState.Stopped;
+    }
+
+    fun rewindPlayback() {
+        stopPlayback(true); // Stop the playback and rewind
+        mediaPlayerStateLiveData.value = MediaPlayerState.Idle;
+    }
 }
