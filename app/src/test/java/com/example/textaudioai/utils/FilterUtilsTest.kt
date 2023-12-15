c32505f95b000405fcbab07a9346d7bd9f40d5ea
Couanon Thibault <103991748+TitiBond@users.noreply.github.com>
2023-12-15 11:06:45 +0100

    feat: adding new test for filter method

--- app/src/test/java/com/example/textaudioai/utils/FilterUtilsTest.kt
+++ app/src/test/java/com/example/textaudioai/utils/FilterUtilsTest.kt
@@ -3,6 +3,9 @@ package com.example.textaudioai.utils
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.textaudioai.repositories.Player
 import com.example.textaudioai.voices.resMockup
+import com.example.textaudioai.voices.sortAscResMockup
+import com.example.textaudioai.voices.sortAscTextResMockup
+import com.example.textaudioai.voices.sortTextRobeResMockup
 import com.example.textaudioai.voices.voicesMockup
 import org.junit.Assert
 import org.junit.Rule
@@ -25,6 +28,70 @@ class FilterUtilsTest {
         //ASSERT
         Assert.assertEquals(
             resMockup,
+            voicesResult
+        )
+    }
+
+    @Test
+    fun `basic text search with robe should return robert and roberto Player`(){
+        //ARRANGE
+        val filterText = "robe"
+        val dateFilterType = DateFilteredType.NONE
+
+        //ACT
+        var voicesResult = filterVoices(voicesMockup,filterText,dateFilterType)
+
+        //ASSERT
+        Assert.assertEquals(
+            sortTextRobeResMockup,
+            voicesResult
+        )
+    }
+
+    @Test
+    fun `basic text search with robe and ASC date filter should return roberto and robert Player`(){
+        //ARRANGE
+        val filterText = "robe"
+        val dateFilterType = DateFilteredType.ASC
+
+        //ACT
+        var voicesResult = filterVoices(voicesMockup,filterText,dateFilterType)
+
+        //ASSERT
+        Assert.assertEquals(
+            sortAscTextResMockup,
+            voicesResult
+        )
+    }
+
+    @Test
+    fun `basic text search with zoubi should return empty list`(){
+        //ARRANGE
+        val filterText = "zoubi"
+        val dateFilterType = DateFilteredType.ASC
+
+        //ACT
+        var voicesResult = filterVoices(voicesMockup,filterText,dateFilterType)
+
+        //ASSERT
+        Assert.assertEquals(
+            listOf<Player>() ,
+            voicesResult
+        )
+    }
+
+    @Test
+    fun `ASC date filter return a sorted list`(){
+        //ARRANGE
+        val filterText = ""
+        val dateFilterType = DateFilteredType.ASC
+
+        //ACT
+        var voicesResult = filterVoices(voicesMockup,filterText,dateFilterType)
+
+        //ASSERT
+        Assert.assertEquals(
+            sortAscResMockup,
             voicesResult
         )
     }
